a6469a7b84f6af61c16d4c8817dd7a82361870d1
Sophie Carr <sophiemc99@gmail.com>
2023-01-18 16:59:06 -0500

    fixed contact page and added helper funct

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
-// import { validateEmail } from '../../utils/helpers';
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
  const [firstName, setFirstName] = useState('');
  const [lastName, setLastName] = useState('');
  const [email, setEmail] = useState('');
  const [message, setMessage] = useState('');
+ const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
     
-    if (inputType === 'firstName') {
+    if (inputType === 'First name') {
       setFirstName(inputValue);
-    } else if (inputType === 'lastName') {
+    } else if (inputType === 'Last name') {
       setLastName(inputValue);
-    } else if (inputType === 'email') {
+    } else if (inputType === 'Email') {
       setEmail(inputValue);
     } else {
       setMessage(inputValue);
     };
+
+    if (!inputValue.length){
+      setErrorMessage(`${inputType} is required ! ! !`)
+      return;
+    } else {
+      setErrorMessage('');
+    }
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Invalid email ! ! !');
+      return;
+    } else {
+      setErrorMessage('');
+    }
 
     alert(`Thank you ${firstName}, your contact info has been submitted.`);
       setFirstName('');
@@ -35,34 +50,34 @@ export default function Contact() {
 
  return (
    <div className='contact-form'>
-     <h1>
-       Hello {firstName} {lastName}
-     </h1>
+     <h3>
+       Hey {firstName}! Please leave your info and a short message and I'll get in contact with you ASAP!
+     </h3>
      <form className="form">
        <input
          value={firstName}
-         name="firstName"
+         name="First name"
          onChange={handleInputChange}
          type="text"
          placeholder="First Name"
        />
        <input
          value={lastName}
-         name="lastName"
+         name="Last name"
          onChange={handleInputChange}
          type="text"
          placeholder="Last Name"
        />
        <input
          value={email}
-         name="email"
+         name="Email"
          onChange={handleInputChange}
          type="email"
          placeholder="Email"
        />
        <input
          value={message}
-         name="message"
+         name="Message"
          onChange={handleInputChange}
          type="text"
          placeholder="Please leave a short message..."
@@ -71,6 +86,11 @@ export default function Contact() {
          Submit
        </button>
      </form>
+     {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
    </div>
  );
 }
